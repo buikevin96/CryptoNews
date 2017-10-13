1260ae866a9e78b5a9131b60c21f18204d3d6d9a
buikevin96@gmail.com <buikevin96@gmail.com>
2017-10-13 01:22:35 -0400

    Added /scrape route

--- server.js
+++ server.js
@@ -8,14 +8,24 @@ var mongoose = require("mongoose");
 var Note = require("./models/Note.js");
 var Article = require("./models/Article.js");
 
+// Scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+
 //Set mongoose to leverage built in Javascript ES6 Promises
 mongoose.Promise = Promise;
+
+// Initialize Express
+var app = express();
 
 // Use morgan and body parser with our app
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
+
+// Make public a static dir
+app.use(express.static("public"));
 
 // Database configuration with mongoose
 mongoose.connect("mongodb://localhost/crypto");
@@ -31,3 +41,46 @@ db.once("open", function(){
     console.log("Mongoose connection successful.");
 });
 
+// Routes
+// =======
+
+// A GET request to scrape the echojs website
+app.get("/scrape", function(req, res){
+    //Grab grab the body  of the html with request
+    request("https://www.reddit.com/r/CryptoCurrency/", function(error, response, html){
+        // Load into cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(html);
+        // Grab every within article tag and do the following
+        $("title").each(function(i, element){
+
+            // Save an empty result object
+            var result = {};
+
+            // Add the text and href of every link, and save them as properties of the result object
+            result.title = $(this).children("a").text();
+            result.link = $(this).children("a").attr("href");
+
+            // Using our Article model, create a new entry
+            // This passes the result object to the entry (and the title and link)
+            var entry = new Article(result);
+
+            // Now, save that entry to the db
+            entry.save(function(err, doc){
+                //Log any errors
+                if (err) {
+                    console.log(err);
+                } 
+                // Or log the doc
+                else {
+                    console.log(doc);
+                }
+            });
+        });
+    });
+    // Tell the browser that we finished scraping the text
+    res.send("Scrape Complete");
+});
+
+app.listen(3000, function(){
+    console.log("App running on port 3000!");
+});
