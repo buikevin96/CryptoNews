1055bd2c68dbb3bbdf0b8298e4f31d97fdf635c2
buikevin96@gmail.com <buikevin96@gmail.com>
2017-10-13 15:22:19 -0400

    Added additional articles routes and Updated index.html

---
+++ public/app.js
@@ -0,0 +1,10 @@
+$.getJSON("/all", function(data){
+    // For each entry of that json...
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+        // Append each of the propties to the table
+        $("#results".append("<tr><td>" + data[i].title + "</td>" +
+                            "<td>" + data[i].link + "</td>" +
+                            "<td>" + data[i].id + "</td></tr>");
+    }
+});
